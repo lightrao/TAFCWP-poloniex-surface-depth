b20fa8f3cfee127c17dd4af980c89df4ae54b26d
lightrao <lightrao@hotmail.com>
2024-07-27 15:30:31 +0800

    Structuring Pair C

--- func_arbitrage_v2.py
+++ func_arbitrage_v2.py
@@ -27,8 +27,6 @@ def structure_triangular_pairs(coin_list):
 
     # Get Pair A
     for pair_a in pairs_list:
-        print("***********")
-        print("pair_a: ", pair_a)
         pair_a_split = pair_a.split("_")
         a_base = pair_a_split[0]
         a_quote = pair_a_split[1]
@@ -48,4 +46,37 @@ def structure_triangular_pairs(coin_list):
                     # here we successfully identified pari and a pair b
 
                     # Get Pair C
-                    print("pair_b: ", pair_b)
+                    for pair_c in pairs_list:
+                        pair_c_split = pair_c.split("_")
+                        c_base = pair_c_split[0]
+                        c_quote = pair_c_split[1]
+
+                        # Count the number of matching C items
+                        if pair_c != pair_a and pair_c != pair_b:
+                            combine_all = [pair_a, pair_b, pair_c]
+                            pair_box = [
+                                a_base,
+                                a_quote,
+                                b_base,
+                                b_quote,
+                                c_base,
+                                c_quote,
+                            ]
+
+                            counts_c_base = 0
+                            for i in pair_box:
+                                if i == c_base:
+                                    counts_c_base += 1
+
+                            counts_c_quote = 0
+                            for i in pair_box:
+                                if i == c_quote:
+                                    counts_c_quote += 1
+
+                            # Determing Triangular Match
+                            if (
+                                counts_c_base == 2
+                                and counts_c_quote == 2
+                                and c_base != c_quote
+                            ):
+                                print(pair_a, pair_b, pair_c)
